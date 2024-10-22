21e9ff9681fd08e645bef7ae547cf5b0533e1fa4
helciosp <helciosp43@gmail.com>
2024-10-22 19:22:23 -0300

    Primeiro commit

---
+++ src/main/java/com/example/ees_higienizacao_app/EesHigienizacaoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ees_higienizacao_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EesHigienizacaoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EesHigienizacaoAppApplication.class, args);
+	}
+
+}
